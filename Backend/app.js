9155631f64d11b2fc04feb1d52742dc9381615dd
BLANC_GUILLAUME <guillaume.blanc92@hotmail.fr>
2023-04-25 19:50:55 +0300

    middlewares+mongoose

--- Backend/app.js
+++ Backend/app.js
@@ -1,32 +1,66 @@
 const express = require('express'); // on require express, précédemment installé
 const app = express(); // on créé une constante app = express puis l'exporter afin de pouvoir l'utiliser depuis d'autres fichiers.
 module.exports = app;
+const mongoose = require('mongoose'); // on installe mongoose pour notre base de données.
+//avec ceci, Express prend toutes les requêtes qui ont comme Content-Type  application/json  et met à disposition leur  body  directement sur l'objet req, ce qui nous permet d'écrire le middleware POST suivant :
+app.use(express.json());
+
+//Connexion à MongoDB
+const password= process.env.DB_PASSWORD // on passe le password présent dans env pour éviter qu'il soit visible
+const username = process.env.DB_USERNAME // on passe également le user name depuis le fichier .env
+const databasename= process.env.DB_DATABASENAME
+
+mongoose.connect(`mongodb+srv://${username}:${password}@${databasename}/?retryWrites=true&w=majority` ,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 
 
-// Middlewares
-/*le premier enregistre « Requête reçue ! » dans la console et passe l'exécution ;
-le deuxième ajoute un code d'état 201 à la réponse et passe l'exécution ;
-le troisième envoie la réponse JSON et passe l'exécution ;
-le dernier élément de middleware enregistre « Réponse envoyée avec succès ! » dans la console.*/
 
+
+//Middlewares// 
+
+// header qui va permettre d'accéder à notre API depuis n'importe quelle origine ( '*' ) ;
+//d'ajouter les headers mentionnés aux requêtes envoyées vers notre API (Origin , X-Requested-With , etc.) ;
+//d'envoyer des requêtes avec les méthodes mentionnées ( GET ,POST , etc.).
 app.use((req, res, next) => {
-    console.log('Requête reçue !');
-    next(); // rajouter next est important pour passer à la prochaine fonction, sans quoi la requête ne se termine jamais
-  });
-
-  app.use((req, res, next) => {
-    res.status(201); // on utilise un code 201 pour la requête http
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
 
-// on utilise la méthode .use, l'application utilisera cette méthode pour tout type de requête
-app.use((req, res) => {
-    res.json({ message: 'Votre requête a bien été reçue !' }); // réponse en json ( objet res.json) :confirmation que notre requête est reçue 
-    next();
- });
 
- // Dans ce cas de figure, on utilise pas next car c'est le dernier middleware, il n'y a plus rien après
- app.use((req, res, next) => {
-    console.log('Réponse envoyée avec succès !');
+  // on utilise la méthode post pour intercepter les requêtes post
+  app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({ // on utilise 201 pour création de ressource
+      message: 'Objet créé !'
+    });
   });
+
+  // on utilise la méthode get pour préciser que nous ne voulons que les requêtes get
+  app.get('/api/stuff', (req, res, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(stuff);
+  });
+
