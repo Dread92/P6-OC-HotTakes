d89a244c00067d49d9f6200e5c9d88629374d01f
Blanc Guillaume <guillaume.blanc92@hotmail.fr>
2023-05-02 18:17:18 +0200

    comments+ jsonwebtoken

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -18,18 +18,25 @@ exports.signup = (req, res, next) => {
   };
 
 // fonction pour connecter un utilisateur existant
-
 exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
         .then(user => {
+            
             if (!user) {
                 return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
             }
+           
             bcrypt.compare(req.body.password, user.password)
                 .then(valid => {
+                     /*Nous utilisons la fonction compare de bcrypt pour comparer le mot de passe entré par l'utilisateur 
+                    avec le hash enregistré dans la base de données*/
                     if (!valid) {
+                        /*S'ils ne correspondent pas, nous renvoyons une erreur401 Unauthorized avec le même message que lorsque l’utilisateur n’a pas été trouvé,
+                        afin de ne pas laisser quelqu’un vérifier si une autre personne est inscrite sur notre site.*/
                         return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
                     }
+                    /*S'ils correspondent, les informations d'identification de notre utilisateur sont valides. 
+                    Dans ce cas, nous renvoyons une réponse 200 contenant l'ID utilisateur et un token*/
                     res.status(200).json({
                         userId: user._id,
                         token: 'TOKEN'
